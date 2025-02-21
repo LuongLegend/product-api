e421888c9fe6162ba5eaf2aa3e3964917a44b6a6
LuongLegend <luongprolv99@gmail.com>
2025-02-21 21:56:54 +0700

    login success return token

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,33 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "12";
 const saltRounds = 10;
 
 const router = express.Router();
 const defaultUser = "admin";
 const defaultPassword = "admin";
 
+function createToken(data) {
+  const token = jwt.sign(data, ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+  return token;
+}
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   let msg = "Login failed!";
-  if ( username !== defaultUser || !password ) return res.json({ msg });
+  if (username !== defaultUser || !password) return res.json({ msg });
   //check pass
   const hashedPassword = bcrypt.hashSync(defaultPassword, saltRounds);
   const isValid = bcrypt.compareSync(password, hashedPassword);
-
+  const response = { msg };
   if (isValid) {
-    msg = "Login success";
+    response.msg = "Login success";
+    const token = createToken({ username });
+    response.token = token;
   }
-
-  return res.json({
-    msg,
-  });
+  //gen TOKEN
+  return res.json(response);
 });
 
 module.exports = router;
