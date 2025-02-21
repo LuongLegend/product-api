77f648884d408100982ad499f032c45476504257
LuongLegend <luongprolv99@gmail.com>
2025-02-21 17:34:25 +0700

    create app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    console.log('hihi');
+    return res.json({msg: 'hello world'}); 
+})
+
+app.listen(PORT, () => console.log(`app is listening port: ${PORT}`));
