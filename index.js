21634dd22926b28fba140916a877ad91f39a027d
LuongLegend <luongprolv99@gmail.com>
2025-02-21 17:39:31 +0700

    add cors, body-parser middle-ware

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config;
+
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
     console.log('hihi');
