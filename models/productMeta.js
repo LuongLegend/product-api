6dde5a154ad24635c25cef2175399cda9a3ef7cb
luonglegend <luongprolv99@gmail.com>
2025-03-13 11:20:45 +0700

    update model

--- models/productMeta.js
+++ models/productMeta.js
@@ -1,22 +1,26 @@
-import { DataTypes } from "sequelize";
-import connectDb from "../config/connectDB.js";
+import { DataTypes } from 'sequelize';
+import connectDb from '../config/connectDB.js';
 
-const productMeta = connectDb.define("product_meta", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true,
-  },
-  productId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  weight: DataTypes.STRING(20),
-  height: DataTypes.STRING(20),
-  color: DataTypes.STRING(20),
-  size: DataTypes.STRING(20),
-  img: DataTypes.STRING(100),
+const productMeta = connectDb.define('product_meta', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    weight: DataTypes.STRING(20),
+    height: DataTypes.STRING(20),
+    color: DataTypes.STRING(20),
+    size: DataTypes.STRING(20),
+    img: DataTypes.STRING(100),
+    quantity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
 });
 
 export default productMeta;
